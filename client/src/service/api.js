977d5518209991f8fdedf9dcc2f29452d7187c7d
luffyguy <harshtanwar25@gmail.com>
2021-08-11 20:52:58 +0530

    updated api link

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -8,7 +8,7 @@ const URL = url;
 export const getImages = (search, count) => {
   try {
     const data = axios.get(
-      `${URL}/?key=${API_KEY}&q=${search}&image_type=photo&per_page=${count}&safesearch=true`
+      `${URL}/?key=${API_KEY}&q=${search}&image_type=all&per_page=${count}&safesearch=true`
     );
     return data;
   } catch (error) {
